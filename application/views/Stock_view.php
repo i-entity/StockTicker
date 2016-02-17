68e365cfc382cf562c3f11743baf110ef07c65c4
George Deng <gndeng@live.com>
2016-02-16 19:21:50 -0800

    basic db table displayed for stocks

---
+++ application/views/Stock_view.php
@@ -0,0 +1,23 @@
+<table style="width:100%">
+
+    <tr>
+        <th>CODE</th>
+        <th>NAME</th> 
+        <th>Category</th> 
+        <th>Value</th>
+    </tr>
+    {stock_array}    
+
+    <tr>
+        
+        
+        <td><a href="/stockhistory/stock/{Name}">{Code}</a></td>
+        <td><a href="/stockhistory/stock/{Name}">{Name}</a></td>
+        <td><a href="/stockhistory/stock/{Name}">{Category}</a></td>
+        <td><a href="/stockhistory/stock/{Name}">{Value}</a></td>
+    </tr>
+
+
+    {/stock_array} 
+
+</table>
